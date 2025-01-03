33e01eccbeb66b7b3415d2d296282fa79a9fe981
aironou <github@aironou.com.br>
2025-01-03 09:58:44 -0300

    copy dbus from lakka

--- packages/sysutils/dbus/package.mk
+++ packages/sysutils/dbus/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dbus"
-PKG_VERSION="1.16.0"
-PKG_SHA256="9f8ca5eb51cbe09951aec8624b86c292990ae2428b41b856e2bed17ec65c8849"
+PKG_VERSION="1.15.8"
+PKG_SHA256="84fc597e6ec82f05dc18a7d12c17046f95bad7be99fc03c15bc254c4701ed204"
 PKG_LICENSE="GPL"
 PKG_SITE="https://dbus.freedesktop.org"
 PKG_URL="https://dbus.freedesktop.org/releases/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="meson:host gcc:host expat systemd"
+PKG_DEPENDS_TARGET="toolchain expat systemd"
 PKG_LONGDESC="D-Bus is a message bus, used for sending messages between applications."
 
 PKG_MESON_OPTS_TARGET="--libexecdir=/usr/lib/dbus \
@@ -16,7 +16,7 @@ PKG_MESON_OPTS_TARGET="--libexecdir=/usr/lib/dbus \
                        -Dapparmor=disabled \
                        -Dasserts=false \
                        -Dchecks=true \
-                       -Dintrusive_tests=false \
+                       -Dembedded_tests=false \
                        -Dinstalled_tests=false \
                        -Dmodular_tests=disabled \
                        -Dxml_docs=disabled \
@@ -40,8 +40,8 @@ post_makeinstall_target() {
 
 post_install() {
   add_user dbus x 81 81 "System message bus" "/" "/bin/sh"
-  add_group dbus 81
-  add_group netdev 497
+  add_group dbus 81 ${DISTRO}
+  add_group netdev 497 ${DISTRO}
 
   echo "chmod 4750 ${INSTALL}/usr/lib/dbus/dbus-daemon-launch-helper" >>${FAKEROOT_SCRIPT}
   echo "chown 0:81 ${INSTALL}/usr/lib/dbus/dbus-daemon-launch-helper" >>${FAKEROOT_SCRIPT}
